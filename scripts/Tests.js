1cd66c5fb7417a7bc2dc6f8906b9fc37c83dde42
borfloave <92087473+borfloave@users.noreply.github.com>
2024-11-10 16:44:07 +0100

    Added random test for testCalcType

--- scripts/Tests.js
+++ scripts/Tests.js
@@ -1,3 +1,4 @@
+import { PokemonTeam } from "./PokemonTeam.js";
 import { calcTypes } from "./TypeCalcs.js";
 
 export function loadTests() {
@@ -13,15 +14,22 @@ async function testCalcTypes() {
     let ground = "Ground";
     let badtype = "Amogus";
 
+    const teamAllay = new PokemonTeam();
+    await teamAllay.generateTeam(false);
+    let pokemon1type = teamAllay.team[0].type1;
+    let pokemon2type = teamAllay.team[2].type1;
+
     let value1 = await calcTypes(water, fire);
     let value2 = await calcTypes(ice, fire);
     let value3 = await calcTypes(ice, normal);
-    let value4 = await calcTypes(electric, ground)
-    let value5 = await calcTypes(electric, badtype)
+    let value4 = await calcTypes(electric, ground);
+    let value5 = await calcTypes(electric, badtype);
+    let value6 = await calcTypes(pokemon1type, pokemon2type);
     console.log("(WATER-FIRE) Expected value : x2 | Test Value: x" + value1);
     console.log("(ICE-FIRE) Expected value : x0.5 | Test Value: x" + value2);
     console.log("(ICE-NORMAL) Expected value : x1 | Test Value: x" + value3);
     console.log("(ELECTRIC-GROUND) Expected value : x0 | Test Value: x" + value4);
     console.log("(ELECTRIC-BADTYPE) Expected value : -1 | Test Value: " + value5);
+    console.log("(" + pokemon1type + " " + pokemon2type + ") Test Value: " + value6);
 
 }
