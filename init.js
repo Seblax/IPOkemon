d2764ef6c99c49b3b6055f19d72bb4b1cb797b05
Seblax <48605789+Seblax@users.noreply.github.com>
2024-11-11 18:57:21 +0100

    feat: me cago en todo willy

--- init.js
+++ init.js
@@ -1,4 +1,4 @@
-import { DrawPokemonSprite } from "./scripts/DrawSprites/PokemonSprites.js";
+import { AnimatePokemon, AppearPokemon, DrawPokemonSprite } from "./scripts/DrawSprites/PokemonSprites.js";
 import { DrawBackground } from "./scripts/DrawSprites/DrawBackGround.js";
 import { Screen } from "./scripts/utils/Screen.js";
 import { PokemonUI } from "./scripts/DrawSprites/PokemonUI.js";
@@ -8,63 +8,72 @@ import { Data, Random } from "./scripts/utils/Data.js";
 import { Music, SetBattleMusic } from "./scripts/utils/Music.js";
 import { DrawMoveSet, init } from "./scripts/DrawSprites/MoveSetSprites.js";
 import { moveSetButtons } from "./scripts/Moves/MovesBehavior.js";
-import { loadMovesFromCSV, loadPokemonFromCSV, loadTypesFromCSV } from "./scripts/utils/CSV.js";
+import {
+  loadMovesFromCSV,
+  loadPokemonFromCSV,
+  loadTypesFromCSV,
+} from "./scripts/utils/CSV.js";
 
-window.onload = function () {
-  (async () => {
-    SetBattleMusic();
-    init();
+async function Start() {
+  SetBattleMusic();
+  init();
 
-    Data.PokemonData = await loadPokemonFromCSV();
-    Data.MovesData = await loadMovesFromCSV();
-    Data.Types = await loadTypesFromCSV();
+  Data.PokemonData = await loadPokemonFromCSV();
+  Data.MovesData = await loadMovesFromCSV();
+  Data.Types = await loadTypesFromCSV();
 
-    const teamAllay = new PokemonTeam();
-    const teamEnemy = new PokemonTeam();
+  const teamAllay = new PokemonTeam();
+  const teamEnemy = new PokemonTeam();
 
-    await teamAllay.generateTeam(false);
-    await teamEnemy.generateTeam(true);
+  await teamAllay.generateTeam(false);
+  await teamEnemy.generateTeam(true);
 
-    const pokemon_1 = teamAllay.team[0];
-    const pokemon_2 = teamEnemy.team[0];
+  const pokemon_1 = teamAllay.team[0];
+  const pokemon_2 = teamEnemy.team[0];
 
-    pokemon_1.hp = Math.round(pokemon_1.totalHp * Random());
-    // pokemon_2.hp = Math.round(pokemon_2.totalHp * Random());
+  pokemon_1.hp = Math.round(pokemon_1.totalHp * Random());
+  // pokemon_2.hp = Math.round(pokemon_2.totalHp * Random());
 
-    Data.ActualAllayPokemon = pokemon_1;
-    Data.ActualEnemyPokemon = pokemon_2;
+  Data.ActualAllayPokemon = pokemon_1;
+  Data.ActualEnemyPokemon = pokemon_2;
 
-    console.log(pokemon_1);
-    console.log(pokemon_2);
+  // console.log(pokemon_1);
+  // console.log(pokemon_2);
 
-    DrawBackground();
+  DrawBackground();
 
-    let menuCanvas = new Screen(".move-set-canvas");
-    let menuSprite = new Sprite(
-      "../assets/sprites/move_set.png",
-      menuCanvas,
-      0,
-      0
-    );
+  let menuCanvas = new Screen(".move-set-canvas");
+  let menuSprite = new Sprite(
+    "../assets/sprites/move_set.png",
+    menuCanvas,
+    0,
+    0
+  );
 
-    menuCanvas.drawCanvas(menuSprite.image, 0, 0);
+  menuCanvas.drawCanvas(menuSprite.image, 0, 0);
 
-    if (pokemon_1.shiny || pokemon_2.shiny) {
-      console.log("Hay un shiny");
-    }
+  if (pokemon_1.shiny || pokemon_2.shiny) {
+    console.log("Hay un shiny");
+  }
 
-    Music();
-    moveSetButtons();
-    UpdateScreen();
-  })();
-};
+  Music();
+  moveSetButtons();
+  UpdateScreen();
 
-export function UpdateScreen(){
-  DrawPokemonSprite(Data.ActualAllayPokemon);
-  DrawPokemonSprite(Data.ActualEnemyPokemon);
+  var allaySprite = DrawPokemonSprite(Data.ActualAllayPokemon);
+  var enemySprite = DrawPokemonSprite(Data.ActualEnemyPokemon);
+
+  AppearPokemon(enemySprite, Data.ActualAllayPokemon );
+  AppearPokemon(allaySprite, Data.ActualEnemyPokemon);
 
   new PokemonUI(Data.ActualAllayPokemon);
   new PokemonUI(Data.ActualEnemyPokemon);
-
+  
   DrawMoveSet(Data.ActualAllayPokemon);
 }
+
+export function UpdateScreen() {
+  requestAnimationFrame(UpdateScreen.bind(this));
+}
+
+Start();
--- scripts/DrawSprites/PokemonSprites.js
+++ scripts/DrawSprites/PokemonSprites.js
@@ -1,38 +1,41 @@
 import { Config } from "../utils/Config.js";
+import { Random, RandomRange } from "../utils/Data.js";
 import { Screen } from "../utils/Screen.js";
 import { Sprite } from "../utils/Sprite.js";
 
 const SpecialPokemon = [
-  6,    //
+  6, //
   // 201,
-  351,  //
-  386,  //
-  412,  //
-  422,  //
-  423,  //
-  479,  //
-  487,  //
-  492,  //
+  351, //
+  386, //
+  412, //
+  422, //
+  423, //
+  479, //
+  487, //
+  492, //
   // 493,
-  555,  //
-  585,  //
-  586,  //
-  641,  //
-  642,  //
-  645,  //
-  646,  //
-  647,  //
-  648, 
-  649
-]
+  555, //
+  585, //
+  586, //
+  641, //
+  642, //
+  645, //
+  646, //
+  647, //
+  648,
+  649,
+];
 
 /**
  * Dibuja el sprite de un Pokémon en la pantalla del juego.
  * @param {object} pokemon - Objeto que representa un Pokémon, que incluye atributos como `enemy`, `mega`, `shiny`, e `id`.
  */
 export function DrawPokemonSprite(pokemon) {
-  const screen = new Screen(".game-canvas"); // Crea una instancia de la clase Screen, asociada al lienzo del juego.
   const isEnemy = pokemon.enemy; // Determina si el Pokémon es un enemigo.
+  const screen = isEnemy
+    ? new Screen(".enemy-canvas")
+    : new Screen(".allay-canvas"); // Crea una instancia de la clase Screen, asociada al lienzo del juego.
 
   // Obtiene la posición de dibujo del Pokémon basada en si es un enemigo o un aliado.
   const pos = isEnemy
@@ -42,8 +45,85 @@ export function DrawPokemonSprite(pokemon) {
   // Obtiene la ruta al sprite del Pokémon.
   var path = GetPokemonSprite(pokemon);
 
+  var sprite = new Sprite(path, screen, pos.x, pos.y);
+  sprite.screen.ctx.translate(pos.x, pos.y);
+  sprite.screen.ctx.imageSmoothingEnabled = false;
+
   // Dibuja el sprite en la pantalla en la posición correspondiente.
-  new Sprite(path, screen, pos.x, pos.y);
+  return sprite;
+}
+
+// Posición inicial de la imagen.
+let x = 0;
+const amplitude = 5; // Amplitud de la onda (altura de la oscilación).
+const frequency = 0.005; // Frecuencia de la onda (controla la longitud de la onda).
+
+// Velocidad de movimiento en el eje X.
+let speed = 2;
+
+export function AnimatePokemon(pokemonSprite) {
+  const ctx = pokemonSprite.screen.ctx;
+
+  // Limpiamos el canvas.
+  ctx.clearRect(0, 0, Config.screen.width, Config.screen.height);
+
+  // Calculamos la posición en el eje Y usando la función seno.
+  const y =
+    -amplitude * Math.sin(pokemonSprite.y * pokemonSprite.x + frequency * x);
+  const angle =
+    0.0002 * Math.sin(pokemonSprite.y * pokemonSprite.x + frequency * x); // 45 grados
+  ctx.rotate(angle);
+
+  // Dibujamos la imagen en la posición actual.
+  ctx.drawImage(pokemonSprite.image, 0, y);
+
+  // Actualizamos la posición en el eje X.
+  // Verificamos si la imagen sale del canvas y la reiniciamos.
+  x += speed;
+
+  // Llamamos a requestAnimationFrame para la siguiente actualización.
+  requestAnimationFrame(() => AnimatePokemon(pokemonSprite));
+}
+
+let AppearFrames = 0;
+
+export function AppearPokemon(pokemonSprite, pokemon) {
+  const ctx = pokemonSprite.screen.ctx;
+  ctx.clearRect(0, 0, Config.screen.width, Config.screen.height);
+
+  AppearSound();
+
+  function AppearSound() {
+    const sound = document.getElementById(pokemon.enemy ? "enemy" : "allay");
+    sound.src = `../assets/music/appears/${pokemon.id}.ogg`; // Establece la fuente de audio (`src`) al valor de `Data.Music`.
+    sound.play();
+  }
+
+  function animate() {
+    ctx.clearRect(0, 0, Config.screen.width, Config.screen.height);
+
+    // Asegúrate de que no se pase de los 2 segundos de animación
+    if (AppearFrames < 0.5 * 60) {
+      // 2 segundos = 120 cuadros si el frameRate es 60fps
+      const y =
+        2 * Math.sin(pokemonSprite.y * pokemonSprite.x + 100 * AppearFrames);
+
+      // Dibujamos la imagen en la posición actual.
+      ctx.drawImage(pokemonSprite.image, 0, y);
+
+      // Actualizamos el contador de frames
+      AppearFrames++;
+
+      // Llamamos a requestAnimationFrame para la siguiente actualización
+      requestAnimationFrame(() => animate());
+    } else {
+      // Cuando los 2 segundos hayan pasado, se reinicia la animación
+      AppearFrames = 0;
+      requestAnimationFrame(() => AnimatePokemon(pokemonSprite)); // Llama a la siguiente fase de animación
+    }
+  }
+
+  animate(); // Inicia la animación
 }
 
 /**
@@ -56,7 +136,7 @@ function GetPokemonSprite(pokemon) {
   var normal = "assets/sprites/pokemons/"; // Ruta base para sprites normales.
   var mega = "assets/sprites/pokemons/megas/"; // Ruta base para sprites de Pokémon megaevolucionados.
 
-  if(SpecialPokemon.includes(pokemon.id)){
+  if (SpecialPokemon.includes(pokemon.id)) {
     return getSpecialSprite(pokemon);
   }
 
@@ -77,12 +157,11 @@ function GetPokemonSprite(pokemon) {
   return path + pokemonSprite;
 }
 
-function getSpecialSprite(pokemon){
+function getSpecialSprite(pokemon) {
   const isEnemy = pokemon.enemy; // Determina si el Pokémon es un enemigo.
   console.log(pokemon.name);
   const tag = pokemon.name.split(" ")[1].toLowerCase();
   var path = "assets/sprites/pokemons/specials/"; // Ruta base para sprites normales.
-
 
   // Agrega la subcarpeta específica dependiendo de si es enemigo o aliado y si es shiny.
   path += isEnemy
@@ -93,5 +172,5 @@ function getSpecialSprite(pokemon){
     ? "back_shiny/"
     : "back/";
 
-  return `${path}${pokemon.id}-${tag}.png`
+  return `${path}${pokemon.id}-${tag}.png`;
 }
